2425ab51824c518eac897cb3a784dacde0bd6e99
Ryry013 <30587035+ryry013@users.noreply.github.com>
2024-10-18 00:12:54 -0700

    Change utils.is_emoji to emoji.is_emoji

--- bot_utils.py
+++ bot_utils.py
@@ -6,6 +6,7 @@ import re
 import shutil
 import sys
 import traceback
+import emoji
 
 from copy import deepcopy
 from datetime import datetime
@@ -319,7 +320,7 @@ def rem_emoji_url(msg):
         msg = msg.content
     new_msg = _emoji.sub('', _url.sub('', msg))
     for char in msg:
-        if is_emoji(char):
+        if emoji.is_emoji(char):
             new_msg = new_msg.replace(char, '').replace('  ', '')
     return new_msg
 
@@ -339,71 +340,6 @@ def get_character_spread(text):
         elif is_english(ch):
             english += 1
     return english, japanese, english + japanese
-
-
-def generous_is_emoji(char):
-    EMOJI_MAPPING = (
-        (0x0080, 0x02AF),
-        (0x0300, 0x03FF),
-        (0x0600, 0x06FF),
-        (0x0C00, 0x0C7F),
-        (0x1DC0, 0x1DFF),
-        (0x1E00, 0x1EFF),
-        (0x2000, 0x209F),
-        (0x20D0, 0x214F),
-        (0x2190, 0x23FF),
-        (0x2460, 0x25FF),
-        (0x2600, 0x27EF),
-        (0x2900, 0x2935),
-        (0x2B00, 0x2BFF),
-        (0x2C60, 0x2C7F),
-        (0x2E00, 0x2E7F),
-        (0x3000, 0x303F),
-        (0xA490, 0xA4CF),
-        (0xE000, 0xF8FF),
-        (0xFE00, 0xFE0F),
-        (0xFE30, 0xFE4F),
-        (0x2757, 0x2757),
-        (0x1F000, 0x1F02F),
-        (0x1F0A0, 0x1F0FF),
-        (0x1F100, 0x1F64F),
-        (0x1F680, 0x1F6FF),
-        (0x1F910, 0x1F96B),
-        (0x1F980, 0x1F9E0),
-    )
-    return any(start <= ord(char) <= end for start, end in EMOJI_MAPPING)
-
-
-def is_emoji(char):
-    EMOJI_MAPPING = (
-        # (0x0080, 0x02AF),
-        # (0x0300, 0x03FF),
-        # (0x0600, 0x06FF),
-        # (0x0C00, 0x0C7F),
-        # (0x1DC0, 0x1DFF),
-        # (0x1E00, 0x1EFF),
-        # (0x2000, 0x209F),
-        # (0x20D0, 0x214F),
-        # (0x2190, 0x23FF),
-        # (0x2460, 0x25FF),
-        # (0x2600, 0x27EF),
-        # (0x2900, 0x2935),
-        # (0x2B00, 0x2BFF),
-        # (0x2C60, 0x2C7F),
-        # (0x2E00, 0x2E7F),
-        # (0x3000, 0x303F),
-        (0xA490, 0xA4CF),
-        (0xE000, 0xF8FF),
-        (0xFE00, 0xFE0F),
-        (0xFE30, 0xFE4F),
-        (0x1F000, 0x1F02F),
-        (0x1F0A0, 0x1F0FF),
-        (0x1F100, 0x1F64F),
-        (0x1F680, 0x1F6FF),
-        (0x1F910, 0x1F96B),
-        (0x1F980, 0x1F9E0),
-    )
-    return any(start <= ord(char) <= end for start, end in EMOJI_MAPPING)
 
 
 def is_ignored_emoji(char):
