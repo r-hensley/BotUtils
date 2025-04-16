43c62b828f1e8f429a299d9f5656895f4d35a233
Ryan H <30587035+ryry013@users.noreply.github.com>
2025-04-15 20:51:52 -0700

    Skip BotUtils test suite for forks that don't have the test directory

--- bot_utils.py
+++ bot_utils.py
@@ -70,15 +70,19 @@ def setup(bot, loop):
     else:
         pass
 
-    test_module = importlib.import_module("cogs.utils.BotUtils.tests.test_bot_utils")
-    test_module = importlib.reload(test_module)
-    suite = unittest.TestLoader().loadTestsFromModule(test_module)
-    test_runner = unittest.TextTestRunner(verbosity=1)
-    # Verbosity:
-    # 0 (quiet): you just get the total numbers of tests executed and the global result
-    # 1 (default): you get the same plus a dot for every successful test or a F for every failure
-    # 2 (verbose): you get the help string of every test and the result
-    test_runner.run(suite)
+    try:
+        test_module = importlib.import_module("cogs.utils.BotUtils.tests.test_bot_utils")
+    except ModuleNotFoundError:
+        print("Skipping BotUtils test suite")
+    else:
+        test_module = importlib.reload(test_module)
+        suite = unittest.TestLoader().loadTestsFromModule(test_module)
+        test_runner = unittest.TextTestRunner(verbosity=1)
+        # Verbosity:
+        # 0 (quiet): you just get the total numbers of tests executed and the global result
+        # 1 (default): you get the same plus a dot for every successful test or a F for every failure
+        # 2 (verbose): you get the help string of every test and the result
+        test_runner.run(suite)
 
 
 # credit: https://gist.github.com/dperini/729294
